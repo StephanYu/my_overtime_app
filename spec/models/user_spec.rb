d95ff13ba46d149d834d7afed75d74fcc7541aad
Stephan Yu <Stephan.yu@gmail.com>
2016-11-22 10:40:09 +0100

    Add validation to user and create corresponding spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User' do
+  describe 'creation' do
+    context 'with valid credentials' do 
+      it 'saves the user' do 
+        create_valid_user
+        expect(@user).to be_valid      
+      end
+    end 
+
+    context 'with invalid credentials' do 
+      it 'does not save the user' do
+        create_invalid_user
+        expect(@user).not_to be_valid
+      end
+    end
+
+    def create_valid_user
+      @user = User.create(first_name: 'Adam', last_name: 'Test', email: 'adam@test.com', password: '123456', password_confirmation: '123456')
+    end
+
+    def create_invalid_user
+      @user = User.create(first_name: '', last_name: '', email: 'nil@test.com', password: '123456', password_confirmation: '123456')
+    end
+  end
 end
