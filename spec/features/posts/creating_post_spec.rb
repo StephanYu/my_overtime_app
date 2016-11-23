f905d947c1cfab2fb812d3a25bbf238b68a1e42f
Stephan Yu <Stephan.yu@gmail.com>
2016-11-23 16:37:40 +0100

    Implement new post spec

--- spec/features/posts/creating_post_spec.rb
+++ spec/features/posts/creating_post_spec.rb
@@ -4,14 +4,24 @@ feature 'Create a new post' do
   before do 
     @user = create(:user)
     login_as(@user)
-    visit new_post_path
+  end
+
+  scenario 'clicking the new post link is successful' do 
+    visit root_path
+
+    click_link('new_post')
+    expect(current_path).to eq(new_post_path)
   end
 
   scenario 'the new form can be reached' do 
+    visit new_post_path
+
     expect(page.status_code).to eq(200)
   end
 
   scenario 'the post can be created from the form' do 
+    visit new_post_path
+
     fill_in 'post[date]', with: Date.today
     fill_in 'post[rationale]', with: 'A test post'
     click_on 'Save'
@@ -20,6 +30,8 @@ feature 'Create a new post' do
   end
 
   scenario 'the post has a user associated with it' do 
+    visit new_post_path
+
     fill_in 'post[date]', with: Date.today
     fill_in 'post[rationale]', with: 'A test post'
     click_on 'Save'
