f6c0ba5fbf5aa1def9f6c0e9ae2bc17866198bf8
Stephan Yu <Stephan.yu@gmail.com>
2016-11-25 16:51:29 +0100

    Implement hiding of post status on edit form and create partial for status element

--- spec/features/admins/editing_post_status_spec.rb
+++ spec/features/admins/editing_post_status_spec.rb
@@ -2,17 +2,27 @@ require 'rails_helper'
 
 feature 'Editing Post status via form' do 
   before do 
-    @admin_user = create(:admin_user)
     @post = create(:post)
-    login_as(@admin_user)
   end
 
-  scenario 'post is approved by setting its status to approved' do 
+  scenario 'admin user sets post status to approved' do 
+    @admin_user = create(:admin_user)
+    login_as(@admin_user)
+
     visit edit_post_path(@post)
 
     choose('post_status_approved')
     click_on('Save')
 
     expect(@post.reload.status).to eq('approved')
+  end
+
+  scenario 'non-admin user cannot see the status radio buttons' do 
+    @regular_user = create(:user)
+    login_as(@regular_user)
+
+    visit edit_post_path(@post)
+
+    expect(page).not_to have_content('Approved')
   end
 end
