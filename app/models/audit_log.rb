472ac4506bd596206ce183e61fbd1dd6a76cb68e
Stephan Yu <Stephan.yu@gmail.com>
2016-12-10 14:10:25 +0000

    Create callback to set default start date and implement spec

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,4 +1,12 @@
 class AuditLog < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :status, :start_date, :user_id
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.start_date ||= Date.today - 6.days
+  end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -4,4 +4,11 @@ describe 'AuditLog' do
   it { should validate_presence_of(:status) }
   it { should validate_presence_of(:start_date) }
   it { should validate_presence_of(:user_id) }
+
+  it 'has a default start date that commences 6 days prior ' do 
+    user = create :user
+    audit_log = AuditLog.create(user_id: user, start_date: nil)
+    
+    expect(audit_log.start_date).to eq(Date.today - 6.days)
+  end
 end
