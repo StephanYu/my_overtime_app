dfd2fa72b647c7505be10f7f50558c3240a2d092
Stephan Yu <Stephan.yu@gmail.com>
2016-12-10 16:57:22 +0000

    Implement feature that only admin users can view the audit logs

--- spec/features/admins/navigating_to_audit_logs_specs.rb
+++ spec/features/admins/navigating_to_audit_logs_specs.rb
@@ -13,13 +13,25 @@ feature 'Audit Logs' do
     create_audit_log
     login_as_admin_user
     visit audit_logs_path
-    
+
     expect(page).to have_content(/SNOW/)
+  end
+
+  scenario 'non-admin user cannot view audit logs page' do 
+    login_as_regular_user
+    visit audit_logs_path
+
+    expect(current_path).to eq(root_path)
   end
 
   def login_as_admin_user 
     admin_user = create :admin_user
     login_as(admin_user)
+  end
+
+  def login_as_regular_user 
+    regular_user = create :user
+    login_as(regular_user)
   end
 
   def create_audit_log 
